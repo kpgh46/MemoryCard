f6c29d6330de9be1f6895673973a7bddbe81ea78
Kevin McPeak <kevmc46@gmail.com>
2022-05-23 15:17:29 -0400

    created Card component and rendered logos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import boston from "./logos/boston.png";
+import atlanta from "./logos/atlanta.png";
+import brooklyn from "./logos/brooklyn.png";
+import charlotte from "./logos/charlotte.png";
 
 function App() {
+	let [currentScore, setCurrentScore] = React.useState(5);
+	let [highScore, setHighScore] = React.useState(2);
+	let [cards, setCards] = React.useState([
+		{
+			name: "atlanta",
+			id: 1,
+			image: atlanta,
+		},
+		{
+			name: "chicago",
+			id: 2,
+			image: boston,
+		},
+		{
+			name: "brooklyn",
+			id: 2,
+			image: brooklyn,
+		},
+		{
+			name: "charlotte",
+			id: 2,
+			image: charlotte,
+		},
+	]);
+
+	let allCards = cards.map((item) => {
+		return <Card image={item.image} />;
+	});
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload test.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<Header currentScore={currentScore} highScore={highScore} />
+			{allCards}
 		</div>
 	);
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header(props) {
+	return (
+		<div>
+			<header>
+				<h3>NBA Memory Card Game</h3>
+				<div className="scoreboard">
+					<div>Current Score: {props.currentScore}</div>
+					<div>High Score: {props.highScore}</div>
+				</div>
+			</header>
+		</div>
+	);
+}
